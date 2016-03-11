126e13d752e47248e065fac1a3a7a194f9f8a776
Nathan Risto <nathanristo@hotmail.com>
2016-03-11 00:53:07 -0600

    fixed issues found on resharper code inspection

--- Gar.Desktop/Views/DelimetersView.xaml.cs
+++ Gar.Desktop/Views/DelimetersView.xaml.cs
@@ -1,28 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Gar.Desktop.Views
+﻿namespace Gar.Desktop.Views
 {
-    /// <summary>
-    /// Interaction logic for DelimetersView.xaml
-    /// </summary>
-    public partial class DelimetersView : UserControl
+    public partial class DelimetersView
     {
+        #region constructors
+
         public DelimetersView()
         {
             InitializeComponent();
         }
+
+        #endregion
     }
 }
